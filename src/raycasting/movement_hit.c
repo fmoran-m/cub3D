bf5b738ed5527c869a0c43757ca0c3fc5e15eb64
Nerea <nervilbar@gmail.com>
2024-08-29 20:17:08 +0200

    colliders fixed

--- src/raycasting/movement_hit.c
+++ src/raycasting/movement_hit.c
@@ -6,7 +6,7 @@
 /*   By: nvillalt <nvillalt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:27:06 by nvillalt          #+#    #+#             */
-/*   Updated: 2024/08/29 17:44:42 by nvillalt         ###   ########.fr       */
+/*   Updated: 2024/08/29 20:16:13 by nvillalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,27 @@
 int	check_hit_x(t_player *player, t_map *map, double new_pos_x)
 {
 	int	pos_y;
-	int	pos_x;
+	int max_pos_x;
+	int min_pos_x;
 
+	max_pos_x = floor((new_pos_x) + 0.05);
+	min_pos_x = floor((new_pos_x) - 0.05);
 	pos_y = floor(player->pos_y);
-	pos_x = floor(new_pos_x);
-	if (map->map[pos_y][pos_x] == '1')
-		return (0);
-	return (1);
+	if (map->map[pos_y][max_pos_x] == '1' || map->map[pos_y][min_pos_x] == '1')
+		return 0;
+	return 1;
 }
 
 int	check_hit_y(t_player *player, t_map *map, double new_pos_y)
 {
-	int	pos_y;
-	int	pos_x;
+	int	max_pos_y;
+	int	min_pos_y;
+	int pos_x;
 
-	pos_x = (player->pos_x);
-	pos_y = (new_pos_y);
-	if (map->map[pos_y][pos_x] == '1')
-		return (0);
-	return (1);
+	pos_x = floor((player->pos_x));
+	max_pos_y = floor((new_pos_y) + 0.05);
+	min_pos_y = floor((new_pos_y) - 0.05);
+	if (map->map[max_pos_y][pos_x] == '1' || map->map[min_pos_y][pos_x] == '1')
+		return 0;
+	return 1;
 }
